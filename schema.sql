bca1b50b5ed942734356583a08246d4a19cd1937
katiallmachay-boop <katiallmachay@gmail.com>
2026-05-09 16:30:29 +0200

    Añade script SQL del campamento

--- schema.sql
+++ schema.sql
@@ -1,1 +1,17 @@
-CREATE TABLE...
+CREATE TABLE actividades (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_actividad VARCHAR(100) NOT NULL,
+    monitor VARCHAR(100) NOT NULL,
+    edad_minima INT NOT NULL,
+    plazas INT NOT NULL
+);
+
+CREATE TABLE participantes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(150) NOT NULL,
+    edad INT NOT NULL,
+    alergia_alimentaria VARCHAR(200),
+    actividad_id INT,
+    FOREIGN KEY (actividad_id) REFERENCES actividades(id)
+);
